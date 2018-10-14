e163fe5a661ef33bb0a85d6c23c990fa1c9a1f1d
Piotr Wontka <leene4@gmail.com>
2018-10-14 10:28:40 +0200

    Added test proj

---
+++ BackendTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BackendTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
