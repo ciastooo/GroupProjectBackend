45fccc5749b18f8d3a8de550e40597d4a70e763f
Piotr Wontka <leene4@gmail.com>
2018-12-05 23:23:20 +0100

    Added migration with categories inserts

---
+++ GroupProjectBackend/Models/DB/Migrations/20181205221959_CategoriesInserts.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupProjectBackend.Models.DB.Migrations
+{
+    public partial class CategoriesInserts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"insert into Categories (Name, Description) values ('restaurant', 'Restauracja');
+                                   insert into Categories (Name, Description) values ('hair', 'Fryzjer');
+                                   insert into Categories (Name, Description) values ('entertainment', 'Rozrywka');
+                                   insert into Categories (Name, Description) values ('relax', 'Relaks/Spa');
+                                   insert into Categories (Name, Description) values ('viewPoint', 'Punkt widokowy');");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- insertCategories.sql
+++
@@ -1,5 +0,0 @@
-insert into Categories (Name, Description) values ('restaurant', 'Restauracja');
-insert into Categories (Name, Description) values ('hair', 'Fryzjer');
-insert into Categories (Name, Description) values ('entertainment', 'Rozrywka');
-insert into Categories (Name, Description) values ('relax', 'Relaks/Spa');
-insert into Categories (Name, Description) values ('viewPoint', 'Punkt widokowy');
