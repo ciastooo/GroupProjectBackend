67eb41c7d501e5b9c2322e6a0f336220aa098cf8
Piotr Wontka <leene4@gmail.com>
2018-12-16 23:21:57 +0100

    WIP

---
+++ GroupProjectBackend/Controllers/RouteController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GroupProjectBackend.Models.DB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroupProjectBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RouteController : ControllerBase
+    {
+        private readonly GroupProjectDbContext _dbContext;
+
+        public RouteController(GroupProjectDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            
+        }
+    }
+}
---
+++ GroupProjectBackend/Models/Dto/RouteDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace GroupProjectBackend.Models.Dto
+{
+    public class RouteDto
+    {
+        public string Id { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsPublic { get; set; }
+
+        public IList<PlaceDto> Places { get; set; }
+    }
+}
